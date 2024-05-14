05a39d6a3c49e0d607c99659868e14b0d7fbc747
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2024-05-14 11:56:40 +0200

    Add SdI config sets
    Since we want to run the code in different environments
    (development, test, production), it is a good practice to set separate
    variables for each environment.
    
    Co-authored-by: Alex Malaszkiewicz <torrocus@users.noreply.github.com>
    Co-authored-by: Grzeg Lisowski <noplisu@users.noreply.github.com>

---
+++ sdi/config.go
@@ -0,0 +1,51 @@
+package sdi
+
+import "crypto/x509"
+
+// Config represents the configuration structure
+type Config struct {
+	Environment string
+	Host        string
+	Verbose     bool
+	CACert      *x509.CertPool
+}
+
+// SOAPReceiveFileEndpoint returns an endpoint for receive file via SOAP
+func (c Config) SOAPReceiveFileEndpoint() string {
+	return c.Host + "/ricevi_file"
+}
+
+// SOAPReceiveInvoicesEndpoint returns an endpoint for receive invoices via SOAP
+func (c Config) SOAPReceiveInvoicesEndpoint() string {
+	return c.Host + "/RicezioneFatture"
+}
+
+// SOAPReceiveNotificationEndpoint returns an endpoint for receive invoices via SOAP
+func (c Config) SOAPReceiveNotificationEndpoint() string {
+	return c.Host + "/ricevi_notifica"
+}
+
+// SOAPTransmitInvoicesEndpoint returns an endpoint for transmit invoices via SOAP
+func (c Config) SOAPTransmitInvoicesEndpoint() string {
+	return c.Host + "/TrasmissioneFatture"
+}
+
+var (
+	// DevelopmentSdIConfig is the settings for development
+	DevelopmentSdIConfig = Config{
+		Environment: "development",
+		Host:        "http://localhost:8080",
+	}
+
+	// ProductionSdIConfig is the settings for production
+	ProductionSdIConfig = Config{
+		Environment: "production",
+		Host:        "https://servizi.fatturapa.it",
+	}
+
+	// TestSdIConfig is the settings for test
+	TestSdIConfig = Config{
+		Environment: "test",
+		Host:        "https://testservizi.fatturapa.it",
+	}
+)
